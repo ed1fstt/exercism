a7611d4e27f640e240a773e57ca53b65454f86ed
Shamil Kurmaev <shamilkurmaev@gmail.com>
2017-10-31 21:18:11 +0500

    add hamming task

--- exercism/java/pangram/src/main/java/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        PangramChecker pangramChecker = new PangramChecker();
-        //System.out.println(pangramChecker.isPangram("The quick brown fox jumps over the lazy dog"));
-        System.out.println(pangramChecker.isPangram(" uick brown fox jumps over the 2 lazy dogs"));
-    }
-}
--- exercism/java/rna-transcription/src/main/java/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        RnaTranscription rnaTranscription = new RnaTranscription();
-        System.out.println(rnaTranscription.transcribe("GATGGAACTTGACTACGTAAATU"));
-    }
-}
