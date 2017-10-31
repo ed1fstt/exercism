4e8aea20d83b1ac0787c176c77b68958c8b33aa6
Shamil Kurmaev <shamilkurmaev@gmail.com>
2017-10-31 20:45:44 +0500

    this is my exercism

---
+++ exercism/java/rna-transcription/src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        RnaTranscription rnaTranscription = new RnaTranscription();
+        System.out.println(rnaTranscription.transcribe("GATGGAACTTGACTACGTAAATU"));
+    }
+}
