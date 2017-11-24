9478327fadf9162f50d109a0f663be061149d941
Shamil Kurmaev <shamilkurmaev@gmail.com>
2017-11-25 04:47:00 +0500

    completed hamming task

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -1,11 +1,21 @@
 public class Hamming {
-
+    private static  String lStrand;
+    private static String rStrand;
     Hamming(String leftStrand, String rightStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (leftStrand.length()!=rightStrand.length()){
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
+        lStrand = leftStrand;
+        rStrand = rightStrand;
     }
 
     int getHammingDistance() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        int hammingDistance = 0;
+        for (int i=0;i<lStrand.length();i++){
+            if (lStrand.charAt(i)!=rStrand.charAt(i)){
+                hammingDistance++;
+            }
+        }
+        return hammingDistance;
     }
-
 }
