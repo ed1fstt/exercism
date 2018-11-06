a7e997f64a6db219d84d6c1462c73707a48bd971
Shamil Kurmaev <shamilkurmaev@gmail.com>
2018-11-07 00:19:44 +0500

    07112018 completed scrabble task in first iteration

--- java/scrabble-score/src/main/java/Main.java
+++ java/scrabble-score/src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main{
     public static void main(String[] args){
-        //System.out.print("test");
-      //  Scr
+        Scrabble scrabble = new Scrabble("a");
+        System.out.println(scrabble.getScore());
     }
 }
--- java/scrabble-score/src/main/java/Scrabble.java
+++ java/scrabble-score/src/main/java/Scrabble.java
@@ -1,12 +1,45 @@
-class Scrabble {
-
-    Scrabble(String word) {
-        //throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-        
+import java.util.Arrays;
+public class Scrabble {
+    private static String input;
+    Scrabble(String finput){
+        input =finput;
     }
-
     int getScore() {
-        //throw new UnsupportedOperationException("Delete this statement and write your own implementation.");/
-    }
+        int rezScore =0;
+        Character ar1[] = {'a','e','i','o','u','l','n','r','s','t'};
+        Character ar2[] = {'d','g'};
+        Character ar3[] = {'b','c','m','p'};
+        Character ar4 []= {'f','h','v','w','y'};
+        Character ar5 []= {'k'};
+        Character ar8[] = {'j','x'};
+        Character ar10[] = {'q','z'};
 
+        while (input.contains(" ")){
+            input =input.replace(" ","");
+        };
+        input=input.toLowerCase();
+
+        char[] inputAr =input.toCharArray();
+
+        Arrays.sort(inputAr);
+
+        for(int i=0;i<inputAr.length;i++){
+            if (Arrays.asList(ar1).contains(inputAr[i])){
+                rezScore++;
+            }else if (Arrays.asList(ar2).contains(inputAr[i])){
+                rezScore=rezScore+2;
+            }else  if(Arrays.asList(ar3).contains(inputAr[i])){
+                rezScore=rezScore+3;
+            }else if (Arrays.asList(ar4).contains(inputAr[i])){
+                rezScore=rezScore+4;
+            }else if (Arrays.asList(ar5).contains(inputAr[i])){
+                rezScore=rezScore+5;
+            }else if (Arrays.asList(ar8).contains(inputAr[i])){
+                rezScore=rezScore+8;
+            }else if (Arrays.asList(ar10).contains(inputAr[i])){
+                rezScore=rezScore+10;
+            }
+        }
+        return rezScore;
+    }
 }
