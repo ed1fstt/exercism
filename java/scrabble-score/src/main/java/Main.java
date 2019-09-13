bd2cbee33f9cec0696d2de568df00631a756eeb9
ed1fstt <shamilkurmaev@gmail.com>
2019-09-13 17:30:12 +0500

    added main

---
+++ java/scrabble-score/src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("test");
+        Scrabble testScrabble = new Scrabble("a");
+        System.out.println(testScrabble.getScore());
+    }
+}
