c181621362bef96601756dc4937040eac997f5b7
Shamil Kurmaev <shamilkurmaev@gmail.com>
2019-09-14 16:25:48 +0500

    rainbow 1st try

--- java/raindrops/src/main/java/RaindropConverter.java
+++ java/raindrops/src/main/java/RaindropConverter.java
@@ -1,21 +1,19 @@
-import java.util.ArrayList;
-
 public class RaindropConverter {
 
     String convert(int number) {
         String raindropSpeak  = "";
-        ArrayList<Integer> arrl = new ArrayList<Integer>();
-        for (int i = 1; i<=number; i++) {
-            if (number % i ==0) {
-                arrl.add(i);
+            if (number % 3 == 0) {
+                raindropSpeak +="Pling";
             }
-        }
-        if (arrl[2] == 3) {
-            raindropSpeak+="Pling";
-        }
-        else if (arrl[])
+            if (number % 5 == 0) {
+              raindropSpeak += "Plang";
+            }
+            if (number % 7 == 0) {
+              raindropSpeak += "Plong";
+            }
+            if (raindropSpeak == "") {
+              raindropSpeak = Integer.toString(number);
+            }
         return raindropSpeak;
-
     }
-
 }
